71cd480d9dc91472793df00cab181b79f77cd424
Khosro Jadidi <khosro.jadidi.developer@gmail.com>
2024-10-09 00:25:23 +0200

    refactor

--- client/Utilities/Print.cs
+++ client/Utilities/Print.cs
@@ -5,19 +5,22 @@ internal static class Print
     internal static void Info(string message)
     {
         Console.ForegroundColor = ConsoleColor.DarkYellow;
-        Console.WriteLine(message);
-        Console.ResetColor();
+        InternalPrint(message);
     }
     internal static void Success(string message)
     {
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine(message);
-        Console.ResetColor();
+        InternalPrint(message);
     }
 
     internal static void Error(string message)
     {
         Console.ForegroundColor = ConsoleColor.Red;
+        InternalPrint(message);
+    }
+
+    private static void InternalPrint(string message)
+    {
         Console.WriteLine(message);
         Console.ResetColor();
     }
