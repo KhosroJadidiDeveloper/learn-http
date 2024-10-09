dca971a104f9a572e2685f08214a29b76b839a19
Khosro Jadidi <khosro.jadidi.developer@gmail.com>
2024-10-09 21:15:50 +0200

    refactor

---
+++ server/Endpoints/Endpoints.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace server.Endpoints;
+
+internal static class Endpoints
+{
+    internal static void MapGetEndpoints(WebApplication app)
+    {
+        app.MapGet("/success", (ILogger<Program> logger) =>
+        {
+            logger.LogInformation("success GET was called");
+            return Results.Ok("The GET request was successful");
+        });
+
+        app.MapGet("/fail", (ILogger<Program> logger) =>
+        {
+            logger.LogInformation("failure GET was called");
+            return Results.NotFound("The GET request was a failure");
+        });
+
+        app.MapGet("/success/{id}", (string id, [FromQuery]int age, ILogger<Program> logger) =>
+        {
+            logger.LogInformation("success GET was called");
+            var response = JsonSerializer.Serialize(new
+            {
+                id
+            });
+            return Results.Ok(response);
+        });
+
+    }
+}
--- server/Program.cs
+++ server/Program.cs
@@ -1,29 +1,10 @@
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using server.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/success", (ILogger<Program> logger) =>
-{
-    logger.LogInformation("success GET was called");
-    return Results.Ok("The GET request was successful");
-});
-
-app.MapGet("/fail", (ILogger<Program> logger) =>
-{
-    logger.LogInformation("failure GET was called");
-    return Results.NotFound("The GET request was a failure");
-});
-
-app.MapGet("/success/{id}/", (string id, [FromQuery]int age, ILogger<Program> logger) =>
-{
-    logger.LogInformation("success GET was called");
-    var response = JsonSerializer.Serialize(new
-    {
-        id
-    });
-    return Results.Ok(response);
-});
+Endpoints.MapGetEndpoints(app);
 
 app.Run();
