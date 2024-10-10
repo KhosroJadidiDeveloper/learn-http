0a4d99653e5f74fe69aad705fa9f7dfab70e87d5
Khosro Jadidi <khosro.jadidi.developer@gmail.com>
2024-10-10 23:10:14 +0200

    improve get

--- client/Exercises/GetExercises.cs
+++ client/Exercises/GetExercises.cs
@@ -1,7 +1,5 @@
-using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Nodes;
-using System.Text.Json.Serialization;
 using client.Utilities;
 
 // ReSharper disable ReplaceWithPrimaryConstructorParameter
@@ -11,15 +9,14 @@ namespace client.Exercises;
 internal sealed class GetExercises(HttpClient client)
 {
     private readonly HttpClient _client = client;
-    private readonly JsonSerializerOptions _jsonSerializerOptions = new(){WriteIndented = true};
+    private readonly JsonSerializerOptions _jsonSerializerOptions = new() { WriteIndented = true };
 
     public async Task Get(string path)
     {
-        Print.Info($"Making a GET request to {_client.BaseAddress}{path}");
-        var responseMessage = await _client.GetAsync(path);
-
         try
         {
+            Print.Info($"Making a GET request to {_client.BaseAddress}{path}");
+            var responseMessage = await _client.GetAsync(path);
             responseMessage.EnsureSuccessStatusCode();
             var content = await responseMessage.Content.ReadAsStringAsync();
             var jsonNode = JsonNode.Parse(content);
@@ -30,20 +27,18 @@ internal sealed class GetExercises(HttpClient client)
             Print.Success($"Age: {jsonNode?.Root["age"]!}");
             Print.Success($"Status code: {(int)responseMessage.StatusCode}");
         }
-        catch (HttpRequestException e)
+        catch (HttpRequestException e) when (e.Message.Contains(""))
         {
             Print.Error(e.Message);
-            Print.Error($"Status code: {(int)responseMessage.StatusCode}");
+            Print.Error($"Status code: {e.StatusCode}");
         }
         catch (ArgumentNullException e)
         {
             Print.Error(e.Message);
-            Print.Error($"Status code: {(int)responseMessage.StatusCode}");
         }
         catch (Exception e)
         {
             Print.Error($"An unhandled exception was thrown: {e.Message}");
-            Print.Error($"Status code: {(int)responseMessage.StatusCode}");
         }
     }
 }
