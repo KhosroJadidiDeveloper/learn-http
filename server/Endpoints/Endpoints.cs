3d78354c3c0a36a61f93f4d65763bdb003d4518d
jaiko <khosro.jadidi@arbetsformedlingen.se>
2024-10-14 22:50:50 +0200

    #

--- server/Endpoints/Endpoints.cs
+++ server/Endpoints/Endpoints.cs
@@ -22,7 +22,7 @@ internal static class Endpoints
 
     internal static void MapPostEndpoints(WebApplication app)
     {
-        app.MapPost("/", (ILogger<Program> logger, [FromServices] UsersDb UsersDb, [FromBody] User user) =>
+        app.MapPost("/", (ILogger<Program> logger, [FromServices] UsersDb usersDb, [FromBody] User user) =>
         {
             logger.LogInformation("post called with: {User}",user);
             return Results.Created("Created successfully.", new
--- server/Entities/User.cs
+++ server/Entities/User.cs
@@ -1,8 +1,15 @@
+using System.Text.Json;
+
 namespace server.Entities;
 
-internal class User
+internal sealed class User
 {
     internal required int? Id { get; set; }
     internal required string Name { get; set; }
     internal required int Age { get; set; }
+
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this);
+    }
 }
