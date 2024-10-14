011a8f8815c959354a5fffbc6a87f25cf77d37ca
jaiko <khosro.jadidi@arbetsformedlingen.se>
2024-10-14 22:01:28 +0200

    add User

---
+++ server/Entities/User.cs
@@ -0,0 +1,8 @@
+namespace server.Entities;
+
+internal class User
+{
+    internal required int Id { get; set; }
+    internal required string Name { get; set; }
+    internal required int Age { get; set; }
+}
